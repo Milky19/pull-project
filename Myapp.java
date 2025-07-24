8bd935ad3deeed39447e5838726918dc74357c90
kiran <kiran0007@gmail.com>
2025-07-24 14:07:32 +0000

    sub-mul-by-kiran

--- Myapp.java
+++ Myapp.java
@@ -2,5 +2,7 @@ public class Msg {
 	public static void main(String[] args){
 		int a=10, int b=20;
 		System.out.println("Sum is:",+(a+b));
+		System.out.println("differenceis:",+(a-b));
+		System.out.println("multiplication is:",+(a*b));
 	}
 }
